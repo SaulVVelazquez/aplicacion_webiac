79c074ff40330b03f179a86b312ef5a90bc4dae3
SaulVVelazquez <1721110050@utectulancingo.edu.mx>
2024-10-29 18:54:16 +0000

    metodo get listo

--- web/app.py
+++ web/app.py
@@ -1,14 +1,31 @@
 import web
+import requests
 
-urls = (
-    '/', 'Index'
-)
-render = web.template.render('templates')
+API_URL = "https://api-rest-qovm.onrender.com/personas"  
+
+# Rutas 
+urls = ("/", "Index")
+
 app = web.application(urls, globals())
+render = web.template.render("templates/")
 
 class Index:
     def GET(self):
-        return render.index()
+        try:
+            response = requests.get(API_URL)
+            
+            if response.status_code == 200:
+                data = response.json()  
+                print("RESPUESTA API:", data)
+            else:
+                print(f"Error en la API: {response.status_code}")
+                data = []  # En caso de error, se regresa una lista vacía.
+
+        except requests.exceptions.RequestException as e:
+            print(f"ERROR ENCONTRADO: {e}")
+            data = []  # Se maneja la excepción y se sigue con una lista vacía.
+
+        return render.index(data)
 
 if __name__ == "__main__":
     app.run()
